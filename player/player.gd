554d571302c65b6812bb162d87375c04a0eca18c
ryann2222 <90405586+ryann2222@users.noreply.github.com>
2023-08-08 18:23:39 -0400

    movement should be complete
    guarding, perfect guarding, dashing, perfect dashing, attack cancelling a guard, dashing cancelling an attack and guard

--- player/player.gd
+++ player/player.gd
@@ -3,7 +3,7 @@ extends Area2D
 @export var maxSpeed = 500
 @export var acceleration = 4000
 var currentSpeed = 0
-var previousInputDir
+var directionFacing = Vector2.RIGHT
 @export var dashingSpeed = 5000
 @export var hp = 100
 @export var damageOnAttack = 20
@@ -21,10 +21,15 @@ var keepCentered = false
 
 var isInvulnerable = false
 var canMove = true
+
 var isDashing = false
 var canDash = true
-var canPerfectDash = false
+var canDashPerfect = false
 var dashingDir = Vector2.ZERO
+
+var isGuarding = false
+var canGuard = true
+var canGuardPerfect = false
 
 var isAttacking = false
 var canAttack = true
@@ -40,7 +45,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var inputDir = Vector2.ZERO
-	var isMidAction = Input.is_action_just_pressed("RMB") or Input.is_action_pressed("key_space") or isDashing
 	
 	# WASD movement
 	if Input.is_action_pressed("key_w"):
@@ -52,57 +56,88 @@ func _process(delta):
 	if Input.is_action_pressed("key_d"):
 		inputDir += Vector2.RIGHT
 	
-	if inputDir.length() > 0:
+	if inputDir.length() > 1:
 		inputDir = inputDir.normalized()
-		previousInputDir = inputDir
+	
+	if inputDir.length() > 0:
+		directionFacing = inputDir
 	
 	if inputDir.length() == 0 and currentSpeed > 0:
 		currentSpeed = 0
 	# stops WASD movement if dashing, blocking
-	if !isMidAction:
-		# WASD movement
-		if currentSpeed < maxSpeed and inputDir.length() != 0:
-			currentSpeed += acceleration * delta
-		if currentSpeed > maxSpeed:
-			currentSpeed = maxSpeed
-		position += inputDir * currentSpeed * delta
+	# attacking - can hold for continuous attacks
+	if Input.is_action_pressed("LMB") and canAttack and !isDashing:
+		$TimerAttackHitscan.start()
+		isAttacking = true
+		canAttack = false
 		
-		# attacking - can hold for continuous attacks
-		if Input.is_action_pressed("LMB") and canAttack:
-			$TimerAttackHitscan.start()
-			isAttacking = true
-			canAttack = false
+		var mousePosition = get_viewport().get_mouse_position()
+		var attackVector = mousePosition - position
+		attackHitscanInstance.position = attackVector.normalized() * 50
+		attackHitscanInstance.rotation = attackVector.angle() + PI / 2
+		attackHitscanInstance.show()
+		add_child(attackHitscanInstance)
+		# play swing animation when done
+		
+		if isGuarding:
+			isGuarding = false
+			canGuardPerfect = false
+			$TimerGuardPerfect.stop()
+			canMove = true
+	# second statement allows basically buffering a guard
+	elif (Input.is_action_just_pressed("key_space") and canGuard) or (Input.is_action_pressed("key_space") and canGuard and !isGuarding):
+		isGuarding = true
+		canGuardPerfect = true
+		$TimerGuardPerfect.start()
+		canMove = false
+		currentSpeed = 0
+		
+		if isAttacking:
+			isAttacking = false
 			
-			var mousePosition = get_viewport().get_mouse_position()
-			var attackVector = mousePosition - position
-			attackHitscanInstance.position = attackVector.normalized() * 50
-			attackHitscanInstance.rotation = attackVector.angle() + PI / 2
-			attackHitscanInstance.show()
-			add_child(attackHitscanInstance)
-			# play swing animation when done
-	else:
-		# dash
-		if Input.is_action_just_pressed("RMB") and canDash:
-			currentSpeed = maxSpeed
-			$TimerDash.start()
-			isDashing = true
-			canDash = false
-			isInvulnerable = true
-			canPerfectDash = true
-			dashingDir = Vector2.LEFT if inputDir == Vector2.ZERO else inputDir
-			$TimerDashPerfect.start()
-			
-			if isAttacking:
-				isAttacking = false
+			if $TimerAttackHitscan.time_left > 0:
 				remove_child(attackHitscanInstance)
+			if $TimerAttackPostAnimation.time_left > 0:
 				remove_child(attackPostAnimationInstance)
-				$TimerAttackHitscan.stop()
-				$TimerAttackCD.stop()
-				$TimerAttackPostAnimation.stop()
+			
+			$TimerAttackHitscan.stop()
+			$TimerAttackCD.stop()
+			$TimerAttackPostAnimation.stop()
 		
-		# block
-	
-	if $TimerDash.get_time_left() > 0 and isDashing:
+	elif Input.is_action_just_released("key_space"):
+		isGuarding = false
+		canMove = true
+		canAttack = true
+	elif Input.is_action_just_pressed("RMB") and canDash:
+		currentSpeed = maxSpeed
+		$TimerDash.start()
+		isDashing = true
+		canDash = false
+		make_invulnerable($TimerDash.wait_time)
+		canDashPerfect = true
+		dashingDir = -directionFacing if inputDir == Vector2.ZERO else inputDir
+		$TimerDashPerfect.start()
+		
+		# dash-cancelling an attack
+		if isAttacking:
+			isAttacking = false
+			
+			if $TimerAttackHitscan.time_left > 0:
+				remove_child(attackHitscanInstance)
+			if $TimerAttackPostAnimation.time_left > 0:
+				remove_child(attackPostAnimationInstance)
+			
+			$TimerAttackHitscan.stop()
+			$TimerAttackCD.stop()
+			$TimerAttackPostAnimation.stop()
+		
+		# dash-cancelling a guard
+		if isGuarding:
+			isGuarding = false
+			canMove = true
+			canGuardPerfect = false
+			$TimerGuardPerfect.stop()
+	elif $TimerDash.get_time_left() > 0 and isDashing:
 		if inputDir != Vector2.ZERO and dashingDir != inputDir:
 			dashingDir = inputDir
 		# base maxSpeed of walking
@@ -110,7 +145,14 @@ func _process(delta):
 		# added boost due to dash
 		position += dashingDir * $TimerDash.get_time_left() * dashingSpeed * delta
 		currentSpeed = maxSpeed
-		
+	elif canMove and !isGuarding:
+		if currentSpeed < maxSpeed and inputDir.length() != 0:
+			currentSpeed += acceleration * delta
+			if currentSpeed > maxSpeed:
+				currentSpeed = maxSpeed
+			
+		position += inputDir * currentSpeed * delta
+	
 	position.x = clamp(position.x, 0, get_viewport_rect().size.x)
 	position.y = clamp(position.y, 0, get_viewport_rect().size.y)
 
@@ -141,21 +183,6 @@ func _on_timer_attack_post_animation_timeout():
 func _on_timer_attack_cd_timeout():
 	canAttack = true
 
-func _on_timer_invulnerability_timeout():
-	isInvulnerable = false
-
-func reduce_hp(amount):
-	if canPerfectDash:
-		print("perfect dash")
-		# effects of perfect dash here
-	if !isInvulnerable:
-		hp -= amount
-		# if hp <= 0 game_over()
-		
-		isInvulnerable = true
-		$TimerInvulnerability.start()
-		# play hit and invulnerable animation
-
 func _on_area_entered(area):
 	# prevents player from taking damage from their own attack
 	if area != attackHitscanInstance:
@@ -163,6 +190,31 @@ func _on_area_entered(area):
 	# elif area == get_node() enemy attack
 	#	reduce_hp(mob.damageOnAttack)
 
+func reduce_hp(amount):
+	if canDashPerfect:
+		print("perfect dash")
+		# effects of perfect dash here
+	elif canGuardPerfect:
+		print("parried")
+	elif isGuarding:
+		print("guarded")
+	elif !isInvulnerable:
+		hp -= amount
+		# if hp <= 0 game_over()
+		
+		make_invulnerable(2.0)
+		# play hit and invulnerable animation
 
 func _on_timer_dash_perfect_timeout():
-	canPerfectDash = false
+	canDashPerfect = false
+
+
+func _on_timer_guard_perfect_timeout():
+	canGuardPerfect = false
+
+func make_invulnerable(time):
+	$TimerInvulnerability.wait_time = time
+	isInvulnerable = true
+
+func _on_timer_invulnerability_timeout():
+	isInvulnerable = false
